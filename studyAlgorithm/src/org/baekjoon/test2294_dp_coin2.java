61481e3f899824b7cce3df3759e59e5030d12c14
hyemi <devham76@gmail.com>
2019-11-18 16:48:05 +0900

    study dp

--- studyAlgorithm/src/org/baekjoon/test2294_dp_coin2.java
+++ studyAlgorithm/src/org/baekjoon/test2294_dp_coin2.java
@@ -1,3 +1,5 @@
+// 내풀이 틀렸음 . 아래에 정확할 풀이.
+// https://m.blog.naver.com/PostView.nhn?blogId=occidere&logNo=220794872664&proxyReferer=https%3A%2F%2Fwww.google.com%2F
 package org.baekjoon;
 
 import java.util.Scanner;
@@ -30,6 +32,19 @@ public class test2294_dp_coin2 {
 		}
 		System.out.println(answer);
 
+		int i,j, n=sc.nextInt(), k=sc.nextInt();
+		int a[] = new int[n+1], d[] = new int[k+1];
+		
+		for( i = 1; i <= k; i++ ) d[i] = 10001; d[0] = 0;
+		for( i = 1; i <= n; i++ ) a[i] = sc.nextInt();
+		
+		for( i = 1; i <=n; i++)
+			for( j = a[i]; j <= k; j++)
+				d[j] = Math.min(d[j], d[ j - a[i] ] +1);
+		
+		if (d[k] == 100001) d[k] = -1;
+		System.out.println(d[k]);
+		sc.close();
 	}
 
 }
