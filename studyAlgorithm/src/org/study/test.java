ce4628bddac8e83bc34b2d9fe5d3b0b0b2d14c09
hyemi <devham76@gmail.com>
2020-02-17 00:33:48 +0900

    ps

--- studyAlgorithm/src/org/study/test.java
+++ studyAlgorithm/src/org/study/test.java
@@ -1,5 +1,6 @@
 package org.study;
 
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
@@ -9,44 +10,67 @@ public class test {
 
 	public static void main(String[] args) {
 		
-//		Date date = new Date(System.currentTimeMillis());
-//		System.out.println(date.toString());
-//		
-//	    Calendar cal = Calendar.getInstance() ;
-//	    cal.setTime(date);
-//	     
-//	    int dayNum = cal.get(Calendar.DAY_OF_WEEK) ;
-//	    System.out.println(dayNum);
-//	    
-//	   
-//	    int gotoMonday = dayNum==1 ? -7 :(2-dayNum);
-//	    cal.add(Calendar.DATE, gotoMonday);
-//	    System.out.println("->"+cal.getTime());
-//	 // 8. 하루전으로 설정된 날짜를 설정된 format으로 String 타입 변경
+		Date date = new Date("Sat Dec 01 00:00:00 GMT 2012"); 
+		SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd"); 
+		// yyyy-MM-dd HH:mm:ss 
+		String format = formatter.format(date);
+		System.out.println(format);
+//	    Date selectedDate = new Date(System.currentTimeMillis());
+//        Date start, end;
+//        Calendar calendar = Calendar.getInstance() ;
+//        calendar.setTime(selectedDate);
+//        
+//        SimpleDateFormat format = new SimpleDateFormat( "yyyy-MM-dd" );
+//        calendar.setTime(selectedDate);
 //
-//	 //String y_date = formatter.format(cal.getTime());
-
-	    
-	    Date selectedDate = new Date(System.currentTimeMillis());
-        Date start, end;
-        Calendar calendar = Calendar.getInstance() ;
-        calendar.setTime(selectedDate);
-
-        // 선택된 날짜가 포함되어있는 월,일요일 구하기
-        // 선택날짜가 월요일이면 start는 선택날짜
-        int dayNum = calendar.get(Calendar.DAY_OF_WEEK);
-        if (dayNum == 2) {
-            start = selectedDate;
-        }
-        // 아니라면 선택날짜가 포함되어있는 월요일 선택
-        else{
-            int gotoMonday = (dayNum==1) ? -7 :(2-dayNum);
-            calendar.add(Calendar.DATE, gotoMonday);
-            start = (Date) calendar.getTime();
-        }
-        // 종료날짜는 = 선택날짜 + 6 , 일요일까지
-        calendar.add(Calendar.DATE, 6);
-        end = (Date) calendar.getTime();
+//        System.out.println("utilDate : "+selectedDate);
+//        java.util.Date utilDate = new java.util.Date();
+//        java.sql.Date sqlDate = new java.sql.Date(selectedDate.getTime());
+//        System.out.println("sqlDate:"+sqlDate);
+//        
+//			try {
+//				Date day1 = format.parse( "2020-02-10" );
+//				String day1s = format.format(day1);
+//				String day2 = format.format(selectedDate);
+//				int compare = day2.compareTo(day1s);
+//				System.out.println(compare);
+//			} catch (ParseException e) {
+//				// TODO Auto-generated catch block
+//				e.printStackTrace();
+//			}
+//            
+//			
+//      SimpleDateFormat format = new SimpleDateFormat( "yyyy-MM-dd" );
+//      //String date  =  format.format("Mon Feb 10 16:44:47 KST 2020");
+//      try {
+//		Date to = format.parse("Mon Feb 10 16:44:47 KST 2020");
+//		System.out.println("to: ");
+//	} catch (ParseException e) {
+//		// TODO Auto-generated catch block
+//		e.printStackTrace();
+//	}
+//
+//		Calendar calendar = Calendar.getInstance() ;
+//	        //calendar.setTime();
+//	        Date start = (Date) calendar.getTime();
+//	        System.out.println(start);
+               
+//        // 선택된 날짜가 포함되어있는 월,일요일 구하기
+//        // 선택날짜가 월요일이면 start는 선택날짜
+//        int dayNum = calendar.get(Calendar.DAY_OF_WEEK);
+//        if (dayNum == 2) {
+//            start = selectedDate;
+//            System.out.println("start : "+start);
+//        }
+//        // 아니라면 선택날짜가 포함되어있는 월요일 선택
+//        else{
+//            int gotoMonday = (dayNum==1) ? -7 :(2-dayNum);
+//            calendar.add(Calendar.DATE, gotoMonday);
+//            start = (Date) calendar.getTime();
+//        }
+//        // 종료날짜는 = 선택날짜 + 6 , 일요일까지
+//        calendar.add(Calendar.DATE, 6);
+//        end = (Date) calendar.getTime();
 	}
 
 }
---
+++ studyAlgorithm/src/org/tryAgain/test3109_bakery.java
@@ -0,0 +1,60 @@
+
+package org.tryAgain;
+
+import java.util.Scanner;
+
+public class test3109_bakery {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int R = sc.nextInt();
+		int C = sc.nextInt();
+		char[][] arr = new char[R][C];
+		boolean[][] visited = new boolean[R][C];
+		for(int i=0; i<R; i++) {
+			String line = sc.next();
+			visited[i][0] = true;
+			for(int j=0; j<C; j++) {
+				arr[i][j] = line.charAt(j);
+			}
+		}
+		int answer=0;
+		int[] dx = {-1,0,1};
+		int[] dy = {1,1,1};
+		
+		for(int i=0; i<C-1; i++) {
+			for(int j=0; j<R; j++) {
+				if(visited[j][i] == false)
+					continue;
+				
+				for(int k=0; k<3; k++) {
+					int nx = j+dx[k];
+					int ny = i+dy[k];
+					
+					if(nx<0 || nx>=R || ny<0 || ny>=C)
+						continue;
+					
+					if(arr[nx][ny] == 'x')
+						continue;;
+						
+					if(visited[nx][ny] == true)
+						continue;
+					
+					visited[nx][ny] = true;
+					if(i == C-3) answer++; 
+				}
+				
+			}
+		}
+		/*
+		for(int i=0; i<R; i++) {
+			for(int j=0; j<C; j++) {
+				System.out.print(visited[i][j] +" ");
+			}
+			System.out.println();
+		}
+		*/
+		System.out.println(answer);
+	}
+
+}
