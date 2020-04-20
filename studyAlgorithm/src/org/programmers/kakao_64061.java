73d53980667d2a26e375b7ff9c0d813cf98baa30
hyemi <devham76@gmail.com>
2020-04-20 16:59:58 +0900

    ps

---
+++ studyAlgorithm/src/org/programmers/kakao_64061.java
@@ -0,0 +1,13 @@
+package org.programmers;
+
+public class kakao_64061 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+    public int solution(int[][] board, int[] moves) {
+        int answer = 0;
+        return answer;
+    }
+}
