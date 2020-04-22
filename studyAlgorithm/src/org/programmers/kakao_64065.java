8587ee0da3a07b60530c175b4a5ddf572f7bad6e
hyemi <devham76@gmail.com>
2020-04-22 18:53:45 +0900

    kakao

---
+++ studyAlgorithm/src/org/programmers/kakao_64065.java
@@ -0,0 +1,22 @@
+// https://programmers.co.kr/learn/courses/30/lessons/64065
+package org.programmers;
+
+public class kakao_64065 {
+
+	public static void main(String[] args) {
+		
+
+	}
+    public int[] solution(String s) {
+        int[] answer = {};
+        return answer;
+    }
+}
+class tuple{
+	int len;
+	int[] arr;
+	tuple(int len, int[] arr){
+		this.len = len;
+		this.arr = arr;
+	}
+}
